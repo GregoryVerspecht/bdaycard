7f7fd10329175e7ab5c7814e8ffd8f0f173a1f45
Gregory Verspecht <gregory.verspecht@gmail.com>
2024-12-24 22:24:31 +0100

    Update service-worker.js
    nieuwe cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "bday-card-v3.0";
+const cacheName = "bday-card-v3.1"; // Verander de versie wanneer je iets wijzigt
 const assets = [
   "/",
   "/index.html",
@@ -8,15 +8,31 @@ const assets = [
 ];
 
 // Cache de bestanden
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
+});
+
+// Verwijder oude caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== cacheName) {
+            // Verwijder caches die niet overeenkomen met de huidige versie
+            return caches.delete(name);
+          }
+        })
+      );
+    })
   );
 });
 
 // Dien bestanden op vanuit de cache
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
