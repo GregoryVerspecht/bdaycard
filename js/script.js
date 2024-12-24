4345cb87668995f4cc4f535cd7711ecc6448befc
Gregory Verspecht <gregory.verspecht@gmail.com>
2024-12-24 15:46:33 +0100

    init commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("surprise-btn").addEventListener("click", () => {
+    const music = document.getElementById("birthday-music");
+    music.play();
+    alert("🎉 Surprise! Wishing you the best day ever, Hind!");
+  });
+  
