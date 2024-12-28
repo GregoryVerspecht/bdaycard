afdbf555f16ba2ee8e2c9837be4d75cc5018daca
Gregory Verspecht <gregory.verspecht@gmail.com>
2024-12-28 01:12:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "bday-card-v3.4"; // Verander de versie wanneer je iets wijzigt
+const cacheName = "bday-card-v4.0"; // Verander de versie wanneer je iets wijzigt
 const assets = [
   "/",
   "/index.html",
