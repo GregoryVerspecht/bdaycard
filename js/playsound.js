6eff041167c8845c5f4fc5359823767496bcef08
Gregory Verspecht <gregory.verspecht@gmail.com>
2024-12-29 03:11:07 +0100

    Update playsound.js

--- js/playsound.js
+++ js/playsound.js
@@ -3,15 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const userInteracted = sessionStorage.getItem('userInteracted') === 'true';
   
     if (userInteracted) {
-      // Selecteer het audio-element
-      const audio = document.getElementById('birthdayAudio');
-  
-      // Probeer het geluid af te spelen
-      audio.play().then(() => {
-        console.log('Audio speelt af omdat er interactie was.');
-      }).catch((error) => {
-        console.error('Audio kon niet automatisch worden afgespeeld:', error);
-      });
+      playSound();
     } else {
       console.log('Geen interactie gedetecteerd. Geluid wordt niet afgespeeld.');
       const warning = document.createElement('p');
@@ -21,4 +13,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  document.addEventListener('click', (event) => {
+     
+      console.log('Ergens anders op het scherm geklikt, niet op een knop of kaart.');
+      playSound();
+   
+  });
   
+
+  function playSound() {
+    const audio = document.getElementById('birthdayAudio');
+  
+    // Probeer het geluid af te spelen
+    audio.play().then(() => {
+      console.log('Audio speelt af omdat er interactie was.');
+    }).catch((error) => {
+      console.error('Audio kon niet automatisch worden afgespeeld:', error);
+    });
+
+}
