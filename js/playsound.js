630f94dcbb57a6de61f3ae2d7a0754ac2be74211
Gregory Verspecht <gregory.verspecht@gmail.com>
2024-12-29 03:22:13 +0100

    auto sound

--- js/playsound.js
+++ js/playsound.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const audio = document.getElementById('birthdayAudio');
+    
     // Controleer of er interactie heeft plaatsgevonden
     const userInteracted = sessionStorage.getItem('userInteracted') === 'true';
   
     if (userInteracted) {
-      playSound();
+      // Wacht totdat het audiobestand klaar is om afgespeeld te worden
+      audio.addEventListener('canplaythrough', () => {
+        playSound(audio);
+      });
+  
+      // Forceer het laden van het audiobestand
+      audio.load();
     } else {
       console.log('Geen interactie gedetecteerd. Geluid wordt niet afgespeeld.');
       const warning = document.createElement('p');
@@ -12,30 +20,34 @@ document.addEventListener('DOMContentLoaded', () => {
       document.body.appendChild(warning);
     }
   });
-
-  document.addEventListener('click', (event) => {
-     
-      console.log('Ergens anders op het scherm geklikt, niet op een knop of kaart.');
-      playSound();
-   
-  });
-
+  
   // Voeg zowel touchstart als click toe
-  document.addEventListener('touchstart', (event) => {
-    console.log('Touch event gedetecteerd!');
-    event.preventDefault(); // Voorkom de "click"-fallback
-    playSound();
+  document.addEventListener('click', () => {
+    console.log('Klik gedetecteerd.');
+    const audio = document.getElementById('birthdayAudio');
+    playSound(audio);
   });
   
-
-  function playSound() {
+  document.addEventListener('touchstart', (event) => {
+    console.log('Touch gedetecteerd.');
+    event.preventDefault(); // Voorkom de "click"-fallback
     const audio = document.getElementById('birthdayAudio');
+    playSound(audio);
+  });
   
-    // Probeer het geluid af te spelen
-    audio.play().then(() => {
-      console.log('Audio speelt af omdat er interactie was.');
-    }).catch((error) => {
-      console.error('Audio kon niet automatisch worden afgespeeld:', error);
-    });
-
-}
+  function playSound(audio) {
+    if (!audio) return;
+  
+    // Controleer of het audiobestand klaar is
+    if (audio.readyState >= 4) {
+      // Probeer het geluid af te spelen
+      audio.play().then(() => {
+        console.log('Audio speelt succesvol af.');
+      }).catch((error) => {
+        console.error('Audio kon niet automatisch worden afgespeeld:', error);
+      });
+    } else {
+      console.log('Audio is nog niet klaar om afgespeeld te worden.');
+    }
+  }
+  
